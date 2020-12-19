2871263ad8afd60f721e554739b4af9f3ae87d60
Jan Nyman <jan.nyman@controlthings.fi>
2020-12-19 13:46:34 +0200

    Send on/off signals continously, this has been tested with a Nexa MYCR-3500

--- main/gpio_example_main.c
+++ main/gpio_example_main.c
@@ -68,6 +68,103 @@ static void busy_wait(int usec) {
     while (esp_timer_get_time() - timestamp < usec);
 }
 
+
+/* 
+Bit formats, from https://tech.jolowe.se/home-automation-rf-protocols/
+
+'1' bit:
+ _____
+|     |
+|     |
+|     |_____
+
+|-----|-----|
+   T     T
+
+'0' bit:
+ _____
+|     |
+|     |
+|     |_________________________
+
+|-----|-------------------------|
+   T               5T
+
+'SYNC' bit:
+ _____
+|     |
+|     |
+|     |__________________________________________________
+
+|-----|--------------------------------------------------|
+   T                         10T
+
+'PAUSE' bit:
+ _____
+|     |
+|     |
+|     |_______________________ . . . ____
+
+|-----|----------------------- . . . ----|
+   T                40T
+
+T = 250 us
+(5T = 1250 us)
+(10T = 2500 us)
+(40T = 10 ms)
+*/
+
+static const int T = 250;   // usec
+
+static void transmit_sync(void) {
+    //SYNC
+    gpio_set(1);
+    busy_wait(T);
+    gpio_set(0);
+    busy_wait(10*T);
+}
+
+static void transmit_phy_bit(uint8_t phy_bit_value) {
+    
+    gpio_set(1);
+    busy_wait(T);
+    gpio_set(0);
+    if (phy_bit_value) {
+        // "1" bit
+        busy_wait(T);
+    }
+    else {
+        // "0" bit
+        busy_wait(5*T);
+    }
+}
+
+static void transmit_pause(void) {
+    //PAUSE
+    gpio_set(1);
+    busy_wait(T);
+    gpio_set(0);
+    busy_wait(40*T);
+}
+
+static void transmit_logical_bit(uint8_t bit_value) {
+    /* Bitcoding
+The data part on the physical link is coded so that every logical bit is sent as two physical bits, where the second one is the inverse of the first one.
+'0' => '01'
+'1' => '10'
+Example: For the logical datastream 0111, is sent over the air as 01101010. */
+
+    if (bit_value) {
+        /* Logical bit 1 is to be sent */
+        transmit_phy_bit(1);
+        transmit_phy_bit(0);
+    }
+    else {
+        transmit_phy_bit(0);
+        transmit_phy_bit(1);
+    }
+}
+
 void app_main()
 {
     gpio_config_t io_conf;
@@ -114,37 +211,40 @@ void app_main()
     while(1) {
         printf("cnt: %d\n", cnt++);
 
-        //SYNC
-        gpio_set(1);
-        busy_wait(250);
-        gpio_set(0);
-        busy_wait(2500);
+
+        /* 26 bit station id...
+        2^25 (base 10) = 10000000000000000000000000 (base 2) = 0x2000000 (base 16) */
+        const uint32_t station_id = 0x200a55a;
+
+        transmit_sync();
         
-        // "1" bit
-        gpio_set(1);
-        busy_wait(250);
-        gpio_set(0);
-        busy_wait(250);
+        /* Transmit 26 bits of station ID */
+        for (int i = 0; i < 26; i++) {
+            if (station_id & (1 << i)) {
+                transmit_logical_bit(1);
+            }
+            else {
+                transmit_logical_bit(0);
+            }
+        }
 
-        // "0" bit
-        gpio_set(1);
-        busy_wait(250);
-        gpio_set(0);
-        busy_wait(1250);
+        /* Transmit group code (let's try 0) */
+        transmit_logical_bit(0);
 
-        // "1" bit
-        gpio_set(1);
-        busy_wait(250);
-        gpio_set(0);
-        busy_wait(250);
+        /* Transmit the desired state bit */
+        transmit_logical_bit(cnt % 2);
 
-        //PAUSE
-        gpio_set(1);
-        busy_wait(250);
-        gpio_set(0);
-        busy_wait(10000);
+        /* Transmit channel bits, '11' for Nexa */
+        transmit_logical_bit(1);
+        transmit_logical_bit(1);
 
-        vTaskDelay(500 / portTICK_RATE_MS);
+        /* Transmit unit bits: Nexa Unit #1 = 11, #2 = 10, #3 = 01. */
+        transmit_logical_bit(1);
+        transmit_logical_bit(1);
+        
+        transmit_pause();
+
+        vTaskDelay(5000 / portTICK_RATE_MS);
         
     }
 }
